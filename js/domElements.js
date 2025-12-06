110f865ad4bdbd9ce9e6e291317ac59a40cc750e
GIGISTUDIODEVE <srhcv1004@gmail.com>
2025-12-06 16:29:39 +0900

    Add content management page

---
+++ js/domElements.js
@@ -0,0 +1,88 @@
+export const authContainer = document.getElementById('authContainer');
+export const dashboardContainer = document.getElementById('dashboardContainer');
+
+export const pages = {
+    login: document.getElementById('loginPage'),
+    signup: document.getElementById('signupPage'),
+    profile: document.getElementById('profilePage')
+};
+
+export const tabAdmin = document.getElementById('tabAdmin');
+export const tabParticipants = document.getElementById('tabParticipants');
+export const tabScenarios = document.getElementById('tabScenarios');
+export const tabContents = document.getElementById('tabContents');
+export const adminView = document.getElementById('adminView');
+export const participantView = document.getElementById('participantView');
+export const scenarioView = document.getElementById('scenarioView');
+export const contentView = document.getElementById('contentView');
+
+export const loginFormElement = document.getElementById('loginFormElement');
+export const signupFormElement = document.getElementById('signupFormElement');
+
+export const showSignupBtn = document.getElementById('showSignup');
+export const showLoginBtn = document.getElementById('showLogin');
+export const forgotPasswordLink = document.querySelector('.forgot-password');
+export const googleLoginBtn = document.getElementById('googleLogin');
+export const logoutBtn = document.getElementById('logoutBtn');
+
+export const toast = document.getElementById('toast');
+export const toastMessage = document.getElementById('toastMessage');
+
+export const birthDateInput = document.getElementById('signupBirthDate');
+export const ageInput = document.getElementById('signupAge');
+export const conditionInput = document.getElementById('signupConditionInput');
+export const addConditionBtn = document.getElementById('addConditionBtn');
+export const conditionChips = document.getElementById('conditionChips');
+
+export const editProfileBtn = document.getElementById('editProfileBtn');
+export const editProfileModal = document.getElementById('editProfileModal');
+export const closeEditModalBtn = document.getElementById('closeEditModal');
+export const cancelEditBtn = document.getElementById('cancelEdit');
+export const editProfileForm = document.getElementById('editProfileForm');
+export const editFullName = document.getElementById('editFullName');
+export const editEmail = document.getElementById('editEmail');
+export const editAffiliation = document.getElementById('editAffiliation');
+export const editPosition = document.getElementById('editPosition');
+export const editJoinDate = document.getElementById('editJoinDate');
+export const editParticipantCount = document.getElementById('editParticipantCount');
+
+export const participantCountEl = document.getElementById('participantCount');
+export const participantTableBody = document.getElementById('participantTableBody');
+export const addParticipantBtn = document.getElementById('addParticipantBtn');
+export const participantSearchInput = document.getElementById('participantSearch');
+export const participantModal = document.getElementById('participantModal');
+export const participantModalTitle = document.getElementById('participantModalTitle');
+export const closeParticipantModalBtn = document.getElementById('closeParticipantModal');
+export const cancelParticipantBtn = document.getElementById('cancelParticipant');
+export const participantForm = document.getElementById('participantForm');
+export const participantNameInput = document.getElementById('participantName');
+export const participantEmailInput = document.getElementById('participantEmail');
+export const participantBirthDateInput = document.getElementById('participantBirthDate');
+export const participantAgeInput = document.getElementById('participantAge');
+export const participantGenderInput = document.getElementById('participantGender');
+export const participantRegionInput = document.getElementById('participantRegion');
+export const participantStatusInput = document.getElementById('participantStatus');
+export const participantLastSeenInput = document.getElementById('participantLastSeen');
+export const participantNotesInput = document.getElementById('participantNotes');
+export const participantConditionInput = document.getElementById('participantConditionInput');
+export const participantAddConditionBtn = document.getElementById('participantAddConditionBtn');
+export const participantConditionChips = document.getElementById('participantConditionChips');
+
+export const addScenarioBtn = document.getElementById('addScenarioBtn');
+export const scenarioTableBody = document.getElementById('scenarioTableBody');
+export const scenarioModal = document.getElementById('scenarioModal');
+export const scenarioModalTitle = document.getElementById('scenarioModalTitle');
+export const closeScenarioModalBtn = document.getElementById('closeScenarioModal');
+export const cancelScenarioBtn = document.getElementById('cancelScenario');
+export const scenarioForm = document.getElementById('scenarioForm');
+export const scenarioTitleInput = document.getElementById('scenarioTitle');
+export const scenarioDifficultyInput = document.getElementById('scenarioDifficulty');
+export const missionNameInput = document.getElementById('missionNameInput');
+export const addMissionBtn = document.getElementById('addMissionBtn');
+export const addQuestionBtn = document.getElementById('addQuestionBtn');
+export const addAnswerBtn = document.getElementById('addAnswerBtn');
+export const missionQuestionsList = document.getElementById('missionQuestionsList');
+export const missionAnswersList = document.getElementById('missionAnswersList');
+export const missionList = document.getElementById('missionList');
+export const contentTableBody = document.getElementById('contentTableBody');
+export const refreshContentsBtn = document.getElementById('refreshContentsBtn');
